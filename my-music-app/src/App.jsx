7063da99a979f535dd117d48f74b83456ac2000f
PatzPaul <pathefister@gmail.com>
2023-06-25 08:45:56 +0000

    Started Vite-react project

---
+++ my-music-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, Fragment } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+<div className='container'>
+
+
+<div className='player'>
+<audio id='audio-player'></audio>
+<h2 id='music-name'
+</div>
+
+</div>
+
+
+      </>
+      
+      
+      )}
+
+export default App
