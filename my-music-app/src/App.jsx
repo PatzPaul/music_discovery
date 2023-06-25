1f7cfd496ce804737946380429b8a804c286e78e
PatzPaul <pathefister@gmail.com>
2023-06-25 09:55:28 +0000

    Added some styling

--- my-music-app/src/App.jsx
+++ my-music-app/src/App.jsx
@@ -2,43 +2,43 @@ import { useState, Fragment } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Groove from './Groove'
 
 function App() {
   return (
     <>
 
-<div className='container'>
+      <div className='container'>
+        <div className='player'>
+          <audio id='audio-player'></audio>
+          <h2 id='music-name' className='music-name'></h2>
+          <p id='music-author' className='music-author'></p>
+
+          <div className='player-progress'>
+            <div className='progress-values'>
+              <span id='player-current-time'>----:----</span>
+              <span id='player-duration'>----:-----</span>
+            </div>
+            <progress id='palyer-progress'></progress>
+          </div>
+          <div className='player-buttons'>
+            <button id='btn-prev' className='btn btn-prev'>
+              <i className='bi bi-rewind-fill'></i>
+            </button>
+            <button id='btn-play' className='btn btn-play'>
+              <i id='btn-play-icon' className='bi bi-play-fill'></i>
+            </button>
+            <button id='btn-next' className='btn btn-next'>
+              <i className='bi bi-fast-forward-fill'></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+    </>
 
 
-<div className='player'>
-<audio id='audio-player'></audio>
-<h2 id='music-name' className='music-name'></h2>
-<p id='music-author' className='music-author'></p>
-
-<div className='player-progress'>
-  <div className='progress-values'>
-    <span id='player-current-time'>----:----</span>
-    <span id='player-duration'>----:-----</span>
-  </div>
-  <progress id='palyer-progress'></progress>
-</div>
-<div className='player-buttons'>
-  <button id='btn-prev' className='btn btn-prev'>
-    <i className='bi bi-rewind-fill'></i>
-  </button>
-  <button id='btn-play' className='btn btn-play'>
-    <i id='btn-play-icon' className='bi bi-play-fill'></i>
-  </button>
-  <button id='btn-next' className='btn btn-next'>
-    <i className='bi bi-fast-forward-fill'></i>
-  </button>
-</div>
-</div>
-</div>
-
-      </>
-      
-      
-      )}
+  )
+}
 
 export default App
---
+++ my-music-app/src/Groove.jsx
@@ -0,0 +1,9 @@
+ 
+
+
+ function Groove (){
+
+
+ }
+
+ export default Groove
